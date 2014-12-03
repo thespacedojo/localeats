ce8818330a87974350afed7ffd613c831a662833
Josh Owens <joshua.owens@gmail.com>
2014-12-02 19:26:20 -0500

    Get map working

---
+++ client/views/home.js
@@ -0,0 +1,13 @@
+Template.home.rendered = function() {
+  if (Session.get('location')) {
+    latitude = Session.get('location').coords.latitude;
+    longitude = Session.get('location').coords.longitude;
+    var map = L.map('map').setView([latitude, longitude], 13);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+        .openPopup();
+  }
+}
